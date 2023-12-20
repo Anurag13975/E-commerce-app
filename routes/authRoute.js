9a13e1b70d074a28c49495db967fc0f913fda599
Anurag <anuragsinghal6072@gmail.com>
2023-12-21 00:58:54 +0530

    05_login user

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { registerController } from '../controllers/authController.js';
+import { loginController, registerController } from '../controllers/authController.js';
 
 const router = express.Router();
 
 // routing
 // REGISTER || method=POST
 router.post('/register', registerController);
+
+// LOGIN || method=POST
+router.post('/login', loginController);
 
 export default router;
